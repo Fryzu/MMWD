c71d9c6496381ad769dc68d7eefda10580fe5932
Fryzu <bartek.fryz@gmail.com>
2018-11-30 15:58:43 +0100

    merge

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 # sim settings
 
+#test settings
 MAX_DISTANCE = 10
 MAX_TRAFFIC = 10
 LINES_NUMBER = 3
 LINE_LENGTH = 3
 MAP_SIZE = 3
-PENALTY = 0
+PENALTY = 1000
---
+++ tests/test_tabo_algo.py
@@ -0,0 +1,28 @@
+import unittest
+from Solution import Solution
+from City import City
+from TaboAlgo import TaboAlgo
+
+class TestTaboAlgo(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(self):
+        print("\nTabo algo test case\n======================")
+
+        #initializing a test taboalgo
+        city = City()
+        with open('tests/test_city.json', 'r') as r_file:
+            city.importFromJson(r_file)
+        solution = Solution()
+        with open('tests/test_solution.json', 'r') as r_file:
+            solution.importFromJson(r_file)
+        self.taboAlgo = TaboAlgo(city, solution)
+
+    def test_cost_function(self):
+        self.assertEqual(self.taboAlgo.cost, 198)
+
+    def iterate(self):
+        pass
+
+if __name__== "__main__":
+    unittest.main()
