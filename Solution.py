baf026e8b8a9847b7b0990ca60a9cfc2610565cb
Fryzu <bartek.fryz@gmail.com>
2018-11-30 13:56:10 +0100

    solution refactorization

--- Solution.py
+++ Solution.py
@@ -2,6 +2,8 @@ from random import randint
 from math import inf
 from abc import ABC, abstractmethod
 import json
+import settings
+
 
 class ISolution(ABC):
     '''Solution class interface for taboAlgo'''
@@ -11,59 +13,26 @@ class ISolution(ABC):
         pass
 
     @abstractmethod
-    def neighbourhood(self, lines: list):
+    def updateLines(self, lines: list):
         pass
-
-    def getTraffic(self, firstNode, secondNode):
-        return self._conncections[firstNode][secondNode]["traffic"]
-
-    def getDistance(self, firstNode, secondNode):
-        return self._conncections[firstNode][secondNode]["distance"]
-
-    def __str__(self):
-        result = 'CITY MAP\n================='
-        for i in self._conncections:
-            result += '\n'
-            result += str(i)
-        result += '\n'
-        return result
-
-    def printDistance(self):
-        result = 'CITY MAP DISTANCE\n=================\n'
-        for i in range(MAP_SIZE):
-            for j in range(MAP_SIZE):
-                connection = '{:>5}'.format(self._conncections[i][j]["distance"]) 
-                result += connection
-            result += "\n"
-        return result
-
-    def printTraffic(self):
-        result = 'CITY MAP TRAFFIC\n=================\n'
-        for i in range(MAP_SIZE):
-            for j in range(MAP_SIZE):
-                connection = '{:>5}'.format(self._conncections[i][j]["traffic"])
-                result += connection
-            result += "\n"
-        return result
 
 class Solution(ISolution):
     ''' Solution representation '''
 
-    def __init__(self, city):
+    def __init__(self):
 
-        self.city = city
         self.lines = []
 
         #generate list of lines
-        for i in range(0, LINES_NUMBER):
+        for i in range(0, settings.LINES_NUMBER):
 
             #generate random line
             line = []
-            startStop = randint(0, MAP_SIZE-1)
+            startStop = randint(0, settings.MAP_SIZE-1)
             line.append(startStop)
 
-            for i in range(1, LINE_LENGTH):
-                nextStop = (line[i-1] + 1)%LINE_LENGTH
+            for i in range(1, settings.LINE_LENGTH):
+                nextStop = (line[i-1] + 1)%settings.LINE_LENGTH
                 line.append(nextStop)
 
             self.lines.append(line)
@@ -73,8 +42,8 @@ class Solution(ISolution):
         #Value function calculating
         result = 0
         #result = self.city.getTraffic(1, 2)
-        for i in range(0, MAP_SIZE):
-            for j in range(0, MAP_SIZE):
+        for i in range(0, settings.MAP_SIZE):
+            for j in range(0, settings.MAP_SIZE):
                 if i != j:
                     result += self.check(i,j)*self.city.getTraffic(i, j)
         return result
@@ -102,21 +71,24 @@ class Solution(ISolution):
                 else:
                     continue
         if globalmin == inf:##jesli nigdzie nie znaleziono polączenia czyli globalmin  = 0 to wyslij kare
-            return PENALTY
+            return settings.PENALTY
         else:
             return globalmin
 
     def neighbourhood(self):
         pass
 
-    def neighbourhood(self, lines):
-        pass
+    def updateLines(self, lines):
+        self.lines = lines
+
+    def importFromJson(self, jsonString):
+        self.lines = json.load(jsonString)
 
     def exportToJson(self):
-        return json.dumps(self.lines)
+        return json.dumps(self.lines, indent=4, sort_keys=True)
 
     def __str__(self):
-        result = '======SOLUTION======'
+        result = 'SOLUTION\n======================'
         for i in self.lines:
             result += '\n'
             result += "Line" + str(i)
